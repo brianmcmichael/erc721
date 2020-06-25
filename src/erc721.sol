d9b5b23ea84ae9cd4ae259b08ba06b64467a5194
Brian McMichael <brian@brianmcmichael.com>
2020-06-25 13:06:35 -0400

    Reorg and classify

--- src/erc721.sol
+++ src/erc721.sol
@@ -9,28 +9,39 @@
 pragma solidity >0.4.20;
 
 contract ERC721Events {
-    event Approval(address indexed src, address indexed guy, uint256 nft);
     event Transfer(address indexed src, address indexed dst, uint256 nft);
+    event Approval(address indexed src, address indexed guy, uint256 nft);
     event ApprovalForAll(address indexed guy, address indexed op, bool ok);
 }
 
-contract ERC721 is ERC721Events {
+contract ERC721Metadata {
     function name() public view returns (string);
     function symbol() public view returns (string);
     function tokenURI(uint256 nft) external view returns (string);
+}
+
+contract ERC721Enumerable {
     function totalSupply() public view returns (uint256);
     function tokenByIndex(uint256 idx) public view returns (uint256);
     function tokenOfOwnerByIndex(address guy, uint256 idx) public view returns (uint256);
+}
+
+contract ERC721TokenReceiver {
+    function onERC721Received(address op, address src, uint256 nft, bytes what) external returns(bytes4);
+}
+
+contract ERC165 {
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+contract ERC721 is ERC721Events, ERC721Metadata, ERC721Enumerable, ERC721TokenReceiver, ERC165 {
     function balanceOf(address guy) public view returns (uint256);
     function ownerOf(uint256 nft) public view returns (address);
+    function safeTransferFrom(address src, address dst, uint256 nft, bytes what) public payable;
+    function safeTransferFrom(address src, address dst, uint256 nft) public payable;
+    function transferFrom(address src, address dst, uint256 nft) public payable;
     function approve(address guy, uint256 nft) public payable returns (address);
     function setApprovalForAll(address op, bool ok) public;
     function getApproved(uint256 nft) public returns (address);
     function isApprovedForAll(address guy, address op) public view returns (bool);
-    function takeOwnership(uint256 nft) public;
-    function safeTransferFrom(address src, address dst, uint256 nft, bytes what) public payable;
-    function safeTransferFrom(address src, address dst, uint256 nft) public payable;
-    function transferFrom(address src, address dst, uint256 nft) public payable;
-    function supportsInterface(bytes4 interfaceID) external view returns (bool); // ERC165
-    function onERC721Received(address op, address src, uint256 nft, bytes what) external returns(bytes4);
 }
