e11525261795891ff282c0d5dd1d14f83851ebfd
Brian McMichael <brian@brianmcmichael.com>
2020-06-25 12:41:49 -0400

    Update to 2018 Final

--- src/erc721.sol
+++ src/erc721.sol
@@ -11,17 +11,26 @@ pragma solidity >0.4.20;
 contract ERC721Events {
     event Approval(address indexed src, address indexed guy, uint256 nft);
     event Transfer(address indexed src, address indexed dst, uint256 nft);
+    event ApprovalForAll(address indexed guy, address indexed op, bool ok);
 }
 
 contract ERC721 is ERC721Events {
     function name() public view returns (string);
     function symbol() public view returns (string);
+    function tokenURI(uint256 nft) external view returns (string);
     function totalSupply() public view returns (uint256);
+    function tokenByIndex(uint256 idx) public view returns (uint256);
+    function tokenOfOwnerByIndex(address guy, uint256 idx) public view returns (uint256);
     function balanceOf(address guy) public view returns (uint256);
     function ownerOf(uint256 nft) public view returns (address);
-    function approve(address guy, uint256 nft) public view returns (address);
+    function approve(address guy, uint256 nft) public payable returns (address);
+    function setApprovalForAll(address op, bool ok) public;
+    function getApproval(uint256 nft) public returns (address);
+    function isApprovedForAll(address guy, address op) public view returns (bool);
     function takeOwnership(uint256 nft) public;
-    function transfer(address dst, uint256 nft) public;
-    function tokenOfOwnerByIndex(address guy, uint256 idx) public returns (uint256);
-    function tokenMetadata(uint256 nft) public returns (string);
+    function safeTransferFrom(address src, address dst, uint256 nft, bytes what) public payable;
+    function safeTransferFrom(address src, address dst, uint256 nft) public payable;
+    function transferFrom(address src, address dst, uint256 nft) public payable;
+    function supportsInterface(bytes4 interfaceID) external view returns (bool); // ERC165
+    function onERC721Received(address op, address src, uint256 nft, bytes what) external returns(bytes4);
 }
