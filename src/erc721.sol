6e5337d5f937f57c3c01b6e8ba1ff1fa643f5b8e
Brian L. McMichael <brian@brianmcmichael.com>
2020-12-06 19:08:15 -0500

    Update for 0.6.X compat

--- src/erc721.sol
+++ src/erc721.sol
@@ -6,42 +6,42 @@
 // required for copyright to apply.  As a result, this is free and
 // unencumbered software belonging to the public domain.
 
-pragma solidity >0.4.20;
+pragma solidity >=0.6.0;
 
-contract ERC721Metadata {
-    function name() public view returns (string memory);
-    function symbol() public view returns (string memory);
+interface ERC721Metadata {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
     function tokenURI(uint256 nft) external view returns (string memory);
 }
 
-contract ERC721Enumerable {
-    function totalSupply() public view returns (uint256);
-    function tokenByIndex(uint256 idx) public view returns (uint256);
-    function tokenOfOwnerByIndex(address guy, uint256 idx) public view returns (uint256);
+interface ERC721Enumerable {
+    function totalSupply() external view returns (uint256);
+    function tokenByIndex(uint256 idx) external view returns (uint256);
+    function tokenOfOwnerByIndex(address guy, uint256 idx) external view returns (uint256);
 }
 
-contract ERC721Events {
+interface ERC721Events {
     event Transfer(address indexed src, address indexed dst, uint256 nft);
     event Approval(address indexed src, address indexed guy, uint256 nft);
     event ApprovalForAll(address indexed guy, address indexed op, bool ok);
 }
 
-contract ERC721TokenReceiver {
+interface ERC721TokenReceiver {
     function onERC721Received(address op, address src, uint256 nft, bytes calldata what) external returns(bytes4);
 }
 
-contract ERC165 {
+interface ERC165 {
     function supportsInterface(bytes4 interfaceID) external view returns (bool);
 }
 
-contract ERC721 is ERC165, ERC721Events, ERC721TokenReceiver {
-    function balanceOf(address guy) public view returns (uint256);
-    function ownerOf(uint256 nft) public view returns (address);
-    function safeTransferFrom(address src, address dst, uint256 nft, bytes memory what) public payable;
-    function safeTransferFrom(address src, address dst, uint256 nft) public payable;
-    function transferFrom(address src, address dst, uint256 nft) public payable;
-    function approve(address guy, uint256 nft) public payable returns (address);
-    function setApprovalForAll(address op, bool ok) public;
-    function getApproved(uint256 nft) public returns (address);
-    function isApprovedForAll(address guy, address op) public view returns (bool);
+interface ERC721 is ERC165, ERC721Events, ERC721TokenReceiver {
+    function balanceOf(address guy) external view returns (uint256);
+    function ownerOf(uint256 nft) external view returns (address);
+    function safeTransferFrom(address src, address dst, uint256 nft, bytes calldata what) external payable;
+    function safeTransferFrom(address src, address dst, uint256 nft) external payable;
+    function transferFrom(address src, address dst, uint256 nft) external payable;
+    function approve(address guy, uint256 nft) external payable returns (address);
+    function setApprovalForAll(address op, bool ok) external;
+    function getApproved(uint256 nft) external returns (address);
+    function isApprovedForAll(address guy, address op) external view returns (bool);
 }
